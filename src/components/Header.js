a3ad1fa3ca5b4ce1f5290b7f7440b92df74ebc97
Prasanth-Nagulapalli <prasanth.ragava@gmail.com>
2024-03-26 16:30:50 +0530

    all css moved to index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useScreenSize } from "../customHooks/ScreenSizeContext";
+import { NavLink } from "react-router-dom";
 import { NavLogo } from "../utils";
 import { navData } from "../utils/data";
+
 const Header = () => {
+  const { screenWidth } = useScreenSize();
   const [show, setShow] = useState(true);
 
   const showMenu = () => {
@@ -14,35 +17,40 @@ const Header = () => {
   };
 
   return (
-    <header className="main_nav_header">
-      <nav className="_flex_box _max_width_center">
-        <figure className="main_nav_logo">
-          <img loading="lazy" src={NavLogo} alt="Restaurant Logo" />
-        </figure>
+    <>
+      <header className="main_nav_header">
+        <nav className="_flex_box _max_width_center">
+          <figure className="main_nav_logo">
+            <img loading="lazy" src={NavLogo} alt="Restaurant Logo" />
+          </figure>
 
-        <div className="nav_bars">
-          <i
-            onClick={showMenu}
-            className={show ? "fa-solid fa-bars" : "fa-solid fa-times"}
-          ></i>
-        </div>
-        <ul
-          className={
-            show
-              ? "_flex_box main_nav_list"
-              : "_flex_box main_nav_list  showMenu"
-          }
-        >
-          {navData.map((item) => (
-            <li key={item.name}>
-              <Link to={item.path} onClick={handleLink}>
-                {item.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+          <div className="nav_bars">
+            <i
+              onClick={showMenu}
+              className={show ? "fa-solid fa-bars" : "fa-solid fa-times"}
+            ></i>
+          </div>
+          <ul
+            className={
+              show
+                ? "_flex_box main_nav_list"
+                : "_flex_box main_nav_list  showMenu"
+            }
+          >
+            {navData.map((item) => (
+              <li key={item.name}>
+                <NavLink to={item.path} onClick={handleLink}>
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+      <div
+        style={{ marginBottom: screenWidth <= 768 ? "5.4538rem" : "0" }}
+      ></div>
+    </>
   );
 };
 
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,21 +1,37 @@
 import React from "react";
+import { useScreenSize } from "../customHooks/ScreenSizeContext";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
+  const { screenWidth } = useScreenSize();
+
+  const Styles = {
+    background: "#fff",
+    height: screenWidth <= 686 ? "85vh" : "calc(100vh - 5.4538rem)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    lineHeight: screenWidth <= 686 ? "3rem" : "5rem",
+    transform: screenWidth <= 686 ? "translateY(-5rem)" : "none",
+  };
+
   return (
-    <div
-      style={{
-        background: "#fff",
-        width: "100%",
-        height: "25vw",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <h1>404 NotFound</h1>
-      <p style={{ fontSize: "2rem", color: "black",paddingTop:'5px' }}>
-        This page is not Found please go back to Home Page
+    <div className="_max_width_center" style={Styles}>
+      <h1 style={{ fontSize: screenWidth <= 686 ? "2.5rem" : "4rem" }}>
+        404 NotFound
+      </h1>
+      <p
+        style={{
+          fontSize: screenWidth <= 686 ? "1.4rem" : "2rem",
+          color: "black",
+          paddingTop: "5px",
+        }}
+      >
+        please go back to{" "}
+        <Link to={"/"} style={{ textDecoration: "underline" }}>
+          Home Page
+        </Link>
       </p>
     </div>
   );
