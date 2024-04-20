d5d627d65adc3acc91f68c933664c6b3e6ce37de
Prasanth-Nagulapalli <prasanth.ragava@gmail.com>
2024-04-20 22:51:41 +0530

    lenis smooth scroll added

--- src/App.js
+++ src/App.js
@@ -15,13 +15,32 @@
 
 // export default App;
 
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import Loading from "./components/Loading";
+import Lenis from '@studio-freight/lenis'
 const Header = lazy(() => import("./components/Header"));
 const AppRoutes = lazy(() => import("./routes/AppRoutes"));
 const Footer = lazy(() => import("./components/Footer"));
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis()
+
+  // lenis.on('scroll', (e) => {
+  //   console.log(e)
+  // })
+  
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  
+  requestAnimationFrame(raf)
+
+  },[])
+
+  
+
   return (
     <>
     <Suspense fallback={<Loading />}>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,8 @@ const Hero = () => {
           <section className="hero_text _flex_box">
             <header>
               {/* <h1 className="hero_text_h1">Little Lemon</h1> */}
-              <h1 className="hero_text_h1"><ReactTyped strings={["Little Lemon"]} typeSpeed={100} backDelay={5000} backSpeed={100} cursorChar={"_"}   loop/></h1>
+              {/* <h1 className="hero_text_h1"><ReactTyped strings={["Little Lemon"]} typeSpeed={100} backDelay={5000} backSpeed={100} cursorChar={"_"}   loop/></h1> */}
+              <h1 className="hero_text_h1"><ReactTyped strings={["Little Lemon"]} typeSpeed={100}  cursorChar={"_"} /></h1>
               <p className="hero_text_header_p">Chicago</p>
             </header>
             <div className="hero_div">
